e2c03c7af58030749e76bc26a431d686af164066
Mohammad <mohammad.joudy@gmail.com>
2025-06-29 23:24:50 +0200

    config changed

--- config.py
+++ config.py
@@ -3,14 +3,14 @@
 import os
 
 # === Shared Parameters ===
-BASE_PATH = "/content/drive/MyDrive/DeepSeaProject"  # or use os.getcwd() locally
+BASE_PATH = "/content/drive/MyDrive/DeepSeaProject/dataset_seanoe_101899"  # or use os.getcwd() locally
 
 # Select species/classes to include
-SELECTED_CLASSES = ["Bythograeid crab", "Other fish"]
+SELECTED_CLASSES = ["Buccinid snail"]
 SAFE_CLASS_NAMES = "_".join(cls.replace(" ", "_") for cls in SELECTED_CLASSES)
 
 # Paths (dynamically generated)
-DATASET_DIR = os.path.join(BASE_PATH, f"yolo_dataset_{SAFE_CLASS_NAMES}")
+DATASET_DIR = os.path.join(BASE_PATH, f"training_{SAFE_CLASS_NAMES}")
 OUTPUT_DIR = os.path.join(BASE_PATH, f"yolo_output_{SAFE_CLASS_NAMES}")
 CSV_NAME = "raw-dataset.csv"
 CSV_PATH = os.path.join(BASE_PATH, CSV_NAME)
